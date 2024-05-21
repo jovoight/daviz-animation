59fc8fee1a1ed101e5bc544a2714e458b33be79c
Josiah Voight <jovoight@gmail.com>
2024-05-20 19:38:08 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HelpAnimation from './components/HelpAnimation'
+import LoadingAnimation from './components/LoadingAnimation'
+
+function App() {
+
+  return (
+    <>
+      <LoadingAnimation />
+      <HelpAnimation />
+    </>
+  )
+}
+
+export default App
